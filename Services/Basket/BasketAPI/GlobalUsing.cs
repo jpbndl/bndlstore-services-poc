72b21fa1dc3977d7234d472ceb97e6498d049c90
JP <jp.bandalaria@outlook.com>
2025-10-17 20:19:59 +0800

    Added BasketAPI in services

--- Services/Basket/BasketAPI/GlobalUsing.cs
+++ Services/Basket/BasketAPI/GlobalUsing.cs
@@ -5,6 +5,7 @@ global using BasketAPI.Data;
 global using Common.CQRS;
 global using Common.Behaviours;
 global using Common.Exceptions;
+global using Common.Exceptions.Handler;
 
 global using Carter;
 global using Mapster;
